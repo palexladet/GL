0724278ee70c20a8c1a09c02d5e0c07fb6f7bd62
palexladet <pierre-alexandre.ladet@u-psud.fr>
2018-11-13 19:46:20 +0100

    exo43(2)

--- src/main.c
+++ src/main.c
@@ -7,12 +7,12 @@ int s=3, n=1;
 
 int main(void)
 {
-    while (n<8)
+    while (s<9876)
     {
         s=s+3*(n+1);
         n=n+1;
     }
-    printf("sommes si des %d premiers termes: %d",n ,s);
+    printf("sommes s2 des %d premiers termes: %d",n ,s);
 
     return 0;
 }
