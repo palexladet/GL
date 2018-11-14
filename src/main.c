a97d25f545b1ef66ead308ac7259c0e551bba2b2
palexladet <pierre-alexandre.ladet@u-psud.fr>
2018-11-14 20:11:12 +0100

    exo43(2)

--- src/main.c
+++ src/main.c
@@ -1,27 +1,20 @@
-
 //  Copyright (c) 2018 Antoine Tran Tan
-
 //
-
 #include <ansi_c.h>
-
 #include "my_header.h"
-#include "stdlib.h"
 
-int s=0, n=1;
+int s=3, n=1;
+
 int main(void)
 {
-    rand();
-}
-int rand(void)
-
-{
-    while (n<12)
-
+    while (s<9876)
     {
-        m=rand(1,10);
-        s=s+m;
-        n=n+1
-        }
+        s=s+3*(n+1);
+        n=n+1;
+    }
+    printf("sommes si des %d premiers termes: %d",n ,s);
+
+    return 0;
 }
+
 
