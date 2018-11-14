8824262ea2e06c9334db78030d1137bf9e201166
palexladet <pierre-alexandre.ladet@u-psud.fr>
2018-11-14 20:13:41 +0100

    exo43(4)

--- src/main.c
+++ src/main.c
@@ -1,23 +1,15 @@
 //  Copyright (c) 2018 Antoine Tran Tan
 //
 #include <ansi_c.h>
-
 #include "stdlib.h"
 
-int s=0, n=1, m;
+
 
 int main(void)
 {
-    while (n<12)
-    {
-		
-        m=rand() % 10 + 1;
-        s=s+m;
-        n=n+1;
-    }
-	return 0;
-}
 
+
+}
 
 
 
