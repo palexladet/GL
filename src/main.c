8bf2169d2c6b789b1097fc4c57d6137215e7ec13
palexladet <pierre-alexandre.ladet@u-psud.fr>
2018-11-14 20:54:24 +0100

    lancerde_essai

--- src/main.c
+++ src/main.c
@@ -1,24 +1,13 @@
-//  Copyright (c) 2018 Antoine Tran Tan
-//
-#include <ansi_c.h>
-#include "my_header.h"
-#include "stdlib.h"
-
-int s=0, n=1;
-int main(void)
+#include "TP_lib.h"
+void main(void)
 {
-    rand();
+	short resultat, i=0;
+	while(i<10)
+	{
+		resultat = LancerDe ();
+		i++;
+	}
 }
-int rand(void)
-{
-    while (n<12)
-    {
-        m=rand(1,10);
-        s=s+m;
-        n=n+1
-    }
-}
-
 
 
 
