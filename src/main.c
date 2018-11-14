ecea0eb44f029eb9145a70760ab7f1b9986dd351
palexladet <pierre-alexandre.ladet@u-psud.fr>
2018-11-14 20:08:04 +0100

    nimp

--- src/main.c
+++ src/main.c
@@ -1,24 +1,27 @@
+
 //  Copyright (c) 2018 Antoine Tran Tan
+
 //
+
 #include <ansi_c.h>
+
 #include "my_header.h"
 #include "stdlib.h"
 
 int s=0, n=1;
-
 int main(void)
 {
     rand();
 }
 int rand(void)
+
 {
     while (n<12)
+
     {
         m=rand(1,10);
         s=s+m;
         n=n+1
-    }
+        }
 }
-
-
 
