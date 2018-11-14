058338cf92672478f6bdd50bdb6ad37fea2917c5
palexladet <pierre-alexandre.ladet@u-psud.fr>
2018-11-14 20:16:55 +0100

    exo43(4)

--- src/main.c
+++ src/main.c
@@ -1,14 +1,20 @@
 //  Copyright (c) 2018 Antoine Tran Tan
 //
 #include <ansi_c.h>
-#include "stdlib.h"
+#include "my_header.h"
 
-
+int s=0, n=1;
 
 int main(void)
 {
+    while (n<20)
+    {
+        s=s+n*3+n*7;
+        n=n+1;
+    }
+    printf("sommes si des %d premiers termes: %d",n ,s);
 
-
+    return 0;
 }
 
 
