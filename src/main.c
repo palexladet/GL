fac07dc87affdcc30288a7da3d14b2c9f72d6b33
palexladet <pierre-alexandre.ladet@u-psud.fr>
2018-11-15 14:04:00 +0100

    exo43(4)

--- src/main.c
+++ src/main.c
@@ -3,14 +3,34 @@
 #include <ansi_c.h>
 #include "my_header.h"
 
-int s=0, n=1;
+int s=0, n=1, t, c;
 
 int main(void)
 {
     while (n<20)
     {
-        s=s+n*3+n*7;
-        n=n+1;
+        t=3*n;
+        c=7*n;
+        if((t%3) == 0 )
+        {
+            if((c%7) == 0 )
+            {
+                s=s+t+c;
+            }
+            else{
+                s=s+t;
+            }
+        }
+        else{
+            if((c%7) == 0 )
+            {
+                s=s+c;   
+            }
+            else{
+                s=s;
+            }
+        }
+        n=n+1;       
     }
     printf("sommes s4 des %d premiers termes: %d",n ,s);
     getchar();
@@ -18,6 +38,3 @@ int main(void)
 
     return 0;
 }
-
-
-
