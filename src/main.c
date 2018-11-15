670e93d38cfb6aab12ffc3313fdd5440feb3e4ef
palexladet <pierre-alexandre.ladet@u-psud.fr>
2018-11-15 13:53:22 +0100

    exo43(3)

--- src/main.c
+++ src/main.c
@@ -2,24 +2,19 @@
 //
 #include <ansi_c.h>
 #include "my_header.h"
-#include "stdlib.h"
+#include <stdlib.h>
 
-int s=0, n=1;
-int main(void)
-{
-    rand();
-}
-int rand(void)
+int s=0, n=1, m;
+
+void main(void)
 {
     while (n<12)
     {
-        m=rand(1,10);
+        m=rand()% 10+1;
         s=s+m;
-        n=n+1
+        n=n+1;
+		
     }
 }
-
-
-
 
 
