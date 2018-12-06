f1594c45d76e4d7ec58a78cac52189591c5d591f
palexladet <pierre-alexandre.ladet@u-psud.fr>
2018-12-06 01:04:42 +0100

    je ne sais pas quoi faire!!!

--- src/main.c
+++ src/main.c
@@ -11,16 +11,16 @@
 int main(void)
 {
 	
-	char ligne1 [80] = "Nom: pentagone_B" ;
-	char ligne2 [80] = "Nb points: 5" ;
-	char ligne3 [80] = "coordonnees: 10 20 30 20 30 40 43 53 35 65" ;
+	char ligne1[80] = "Nom: pentagone_B" ;
+	char ligne2[80] = "Nb points: 5" ;
+	char ligne3[80] = "coordonnees: 10 20 30 20 30 40 43 53 35 65" ;
 	char x[5];
 	char y[5];
-	char nom_fig [100];
-	char nb_pts [100];
+	char nom_fig[100];
+	char nb_pts[100];
 	sscanf(ligne1,"%s",nom_fig);
-	sscanf(ligne2,"%s",nb_pts);
-	sscanf(ligne3,"[%c/%c]",&x,&y);
+	sscanf(ligne2,"%c %*s",nb_pts);
+	sscanf(ligne3,"%c %*s %1c %*s",&x[5],&y[5]);
 	
 	return 0;
 } 	
